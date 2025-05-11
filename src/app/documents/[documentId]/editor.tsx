efc141a61a036c2d2454397e4f73660bdb882587
Anup-1827 <anupalone.off@gmail.com>
2025-05-11 14:59:03 +0530

    TipTap added List, TaskList, Table, Heading
    Added Image extension

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -2,6 +2,13 @@
 
 import { useEditor, EditorContent } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
+import TaskItem from '@tiptap/extension-task-item'
+import TaskList from '@tiptap/extension-task-list'
+import Table from '@tiptap/extension-table'
+import TableCell from '@tiptap/extension-table-cell'
+import TableHeader from '@tiptap/extension-table-header'
+import TableRow from '@tiptap/extension-table-row'
+import Image from '@tiptap/extension-image'
 
 const Editor = () => {
   const editor = useEditor({
@@ -12,8 +19,38 @@ const Editor = () => {
         } 
             
     },
-    extensions: [StarterKit],
-    content: '<p>Hello World! 🌎️</p>',
+    extensions: [
+        StarterKit,
+        TaskList,
+        TaskItem.configure({
+            nested : true
+        }),
+        Table.configure({
+            resizable: true
+        }),
+        TableHeader,
+        TableRow,
+        TableCell,
+        Image
+    ],
+    immediatelyRender: false, //`immediatelyRender` explicitly to `false` to avoid hydration mismatches.
+    content: `
+        <p>Hello World! 🌎️</p>
+        <table>
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th colspan="3">Description</th>
+            </tr>
+            <tr>
+              <td>Cyndi Lauper</td>
+              <td>Singer</td>
+              <td>Songwriter</td>
+              <td>Actress</td>
+            </tr>
+          </tbody>
+        </table>
+        `,
   })
 
   return(
